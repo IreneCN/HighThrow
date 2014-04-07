c57c2254a8af00a38d17dc0387463065807193fe
Irene <icn2104@columbia.edu>
2014-04-06 21:47:45 -0400

    I've included the images for my assignment into my javascript file.

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * @author Chidinma Irene Nwoye
+ */
+
+
+//Below is an array of my images on how far a human can throw
+var images = ["ahigh_throw_blood.png","bhigh_throw_drumstick.png","chigh_throw_redirector.png","dhigh_throw_gravity.png","ehigh_throw_giraffe.png","fhigh_throw_3.png","ghigh_throw_5.png","ihigh_throw_10.png",
+"jhigh_throw_14.png","khigh_throw_16.png","lhigh_throw_balloon.png"]; 
+
